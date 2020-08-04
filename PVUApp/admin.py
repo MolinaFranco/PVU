8be1379dc34976d848afa627a8729b749f955acc
Edu Ludueña <EduLuduena53@gmail.com>
2020-08-04 13:01:57 -0300

    Admin Registered Data

--- PVUApp/admin.py
+++ PVUApp/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+admin.site.register(Persona)
+admin.site.register(Religion)
+admin.site.register(Domicilio)
+admin.site.register(Chico)
+admin.site.register(Familiar)
+admin.site.register(Administrador)
+admin.site.register(Psicopedagogo)
+admin.site.register(Observación_Psico)
+admin.site.register(Taller)
+admin.site.register(Asistencia)
+admin.site.register(Observacion)
+admin.site.register(Ingrediente)
+admin.site.register(Comida)
+admin.site.register(Menu)
